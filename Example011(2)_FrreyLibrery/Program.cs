f6ec70ef78f9075c8c7cbc6f6024bea8f9f093fa
Tatiana Stepanova <kappro12@yandex.ru>
2023-02-27 18:33:28 +0300

    Полностью разобралась с лекцией 2

---
+++ Example011(2)_FrreyLibrery/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] coll)
+{
+    int length = coll.Length;
+    int index = 0;
+    while (index < length)
+    {
+        coll[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArrey(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+ 
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // Этот прием позволяет увидеть, что элемент который мы ищем в массиве отсутствует
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++; 
+    }
+ return position;
+}
+
+int[] array = new int[10]; // дословно:Создай новый массив в котором будет 10 элементов.по умолчанию он будет заполнен 0
+
+FillArray(array);
+array[5] = 4; // принудительно добавим несколько 4 в наш массив
+array[6] = 4;
+PrintArrey(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
---
+++ Example011_ArreyLibrary/Program.cs
@@ -0,0 +1,72 @@
+﻿// void FillArray(int[] coll)
+// {
+//     int length = coll.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         coll[index] = new Random().Next(1, 10);
+//         index++;
+//     }
+// }
+// void PrintArrey(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < 0)
+//     {
+//         Console.WriteLine(col[position]);
+//         position++;
+//     }
+// }
+
+//  int[] array = new int[10]; // дословно:Создай новый массив в котором будет 10 элементов.по умолчанию он будет заполнен 0
+
+// FillArray(array);
+// PrintArrey(array);
+
+
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+// int IndexOf(int[] collection, int find)
+// {
+//     int count = collection.Length;
+//     int index = 0;
+//     int position = -1;
+//     while(index < count)
+//     {
+//         if(collection[index] == find)
+//         {
+//             position = index;
+//             break;
+//         }
+//         index++;
+//     }
+//     return position;
+// }
+int[] array = new int[10];
+
+FillArray(array);
+// array[4] = 4;
+// array[6] = 4;
+PrintArray(array);
+// Console.WriteLine();
+// int pos = IndexOf(array, 444);
+// Console.WriteLine(pos);
